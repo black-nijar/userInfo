55d14224494c15aa08c532845b4ff57c4606cf6e
Nijarudeen  Rajak <50949038+black-nijar@users.noreply.github.com>
2020-04-23 15:31:06 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGOqL9hVtRMnv7CV5IFugcX8buzxr6czc",
-  authDomain: "userinfo-e4534.firebaseapp.com",
-  databaseURL: "https://userinfo-e4534.firebaseio.com",
-  projectId: "userinfo-e4534",
-  storageBucket: "userinfo-e4534.appspot.com",
-  messagingSenderId: "85071368688",
-  appId: "1:85071368688:web:223cce500dfbbdcc9acd35",
-  measurementId: "G-13QD7S7Z53"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 const Firebase = firebase.initializeApp(firebaseConfig);
 const dataBase = Firebase.database().ref('userInfo')
